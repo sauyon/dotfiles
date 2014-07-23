40e8a57a6b8d38313670ea62d602fbdb315b7cf0
Sauyon Lee <sauyonl@sauyon.com>
2014-07-23 12:33:13 -0700

    update packages

---
+++ .emacs.d/elpa/s-20140714.707/s-autoloads.el
@@ -0,0 +1,18 @@
+;;; s-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("s-pkg.el" "s.el") (21453 17131 372463
+;;;;;;  268000))
+
+;;;***
+
+(provide 's-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; s-autoloads.el ends here

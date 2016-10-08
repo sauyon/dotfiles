3e8db21a9a185e9eef612e296b8388636db073c3
Sauyon Lee <sauyonl@sauyon.com>
2016-10-08 17:00:03 +0100

    remove marmalade because of ssl cert issues

--- .emacs.d/init/pref-init.el
+++ .emacs.d/init/pref-init.el
@@ -22,7 +22,7 @@
 ;; (global-auto-complete-mode t)
 
 ;; Packages!
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+;; (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/"))
 (package-initialize)
 

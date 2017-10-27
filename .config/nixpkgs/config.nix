c2344d5327033300195c2ccf793f35449fa07a68
Sauyon Lee <s@uyon.co>
2017-10-27 13:25:42 +0100

    add some package configs

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -1,3 +1,8 @@
 {
   allowUnfree = true;
+
+  packageOverrides = pkgs: rec {
+    # quodlibet = if pkgs.quodlibet-full != null then pkgs.quodlibet-full.override { webkitgtk = null; } else null;
+    chromium = pkgs.chromium.override { enableNaCl = true; };
+  };
 }

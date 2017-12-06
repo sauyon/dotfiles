acbb8434e566807420bad01746e158d506ff3221
Sauyon Lee <s@uyon.co>
2017-12-06 17:44:16 +0000

    add nix customizations for chromium

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -2,7 +2,17 @@
   allowUnfree = true;
 
   packageOverrides = pkgs: rec {
-    # quodlibet = if pkgs.quodlibet-full != null then pkgs.quodlibet-full.override { webkitgtk = null; } else null;
-    chromium = pkgs.chromium.override { enableNaCl = true; };
+    quodlibet = if pkgs?quodlibet-full then
+      pkgs.quodlibet-full.override { webkitgtk = null; }
+    else null;
+    # chromium = (pkgs.chromium.override {
+    #   enableNaCl = true;
+    # });
+
+    chromium = pkgs.chromium.overrideAttrs (oldAttrs: rec {
+      buildCommand = oldAttrs.buildCommand + ''
+        wrapProgram $out/bin/chromium --add-flags "--device-scale-factor=1.5"
+      '';
+    });
   };
 }

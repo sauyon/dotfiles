0a7413863dba06ae411ae5c89e6e94e7f040bbfe
Sauyon Lee <s@uyon.co>
2018-04-26 18:59:03 +0100

    fix my bell function

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -103,6 +103,7 @@
      ("melpa" . "https://melpa.org/packages/")))
  '(package-selected-packages
    '(ws-butler latex-math-preview latex-unicode-math-mode cargo google-c-style bison-mode rust-mode go-errcheck go-dlv go-complete latex-pretty-symbols smali-mode 2048-game ac-clang ac-emoji ac-haskell-proc achievements ac-html ac-ispell ac-ja ac-js2 ac-math ac-mozc ac-python android-mode auto-complete-sage auto-dictionary cmake-mode color-theme csv-mode dict-tree edit-server ensime flycheck-haskell flycheck-kotlin flycheck-rust go-autocomplete graphviz-dot-mode hardcore-mode haskell-mode helm-nixos-options jabber json-mod json-mode magit magit-find-file magit-gh-pulls mmm-mode mo-git-blame multi-term nixos-options oberon pkgbuild-mode pretty-sha-path protobuf-mode scala-mode scala-mode2 scss-mode smart-tabs-mode solarized-theme systemd toml-mode tuareg web-mode yaml-mode zeitgeist))
+ '(ring-bell-function 'my-bell-function)
  '(rust-indent-offset 2)
  '(show-trailing-whitespace t)
  '(standard-indent 2)

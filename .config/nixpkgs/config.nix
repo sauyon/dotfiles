607e1a0100816b0c0ee94457145785e957322ee7
Sauyon Lee <s@uyon.co>
2018-05-06 09:02:02 +0100

    minor updates

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -2,17 +2,10 @@
   allowUnfree = true;
 
   packageOverrides = pkgs: rec {
-    quodlibet = if pkgs?quodlibet-full then
-      pkgs.quodlibet-full.override { webkitgtk = null; }
-    else null;
-    # chromium = (pkgs.chromium.override {
-    #   enableNaCl = true;
+    # chromium = pkgs.chromium.overrideAttrs (oldAttrs: rec {
+    #   buildCommand = oldAttrs.buildCommand + ''
+    #     wrapProgram $out/bin/chromium --add-flags "--device-scale-factor=1.5"
+    #   '';
     # });
-
-    chromium = pkgs.chromium.overrideAttrs (oldAttrs: rec {
-      buildCommand = oldAttrs.buildCommand + ''
-        wrapProgram $out/bin/chromium --add-flags "--device-scale-factor=1.5"
-      '';
-    });
   };
 }

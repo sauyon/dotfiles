817d1043e6d2529aa8762d198cd07cb79f7d177e
Sauyon Lee <s@uyon.co>
2018-10-27 03:56:29 +0100

    fix nix config

--- .config/nixpkgs/config.nix
+++ .config/nixpkgs/config.nix
@@ -1,10 +1,3 @@
 {
   allowUnfree = true;
-
-  packageOverrides = pkgs: rec {
-    nix = super.nix.override {
-      storeDir = "/users/ug15sl/.local/nix/store";
-      storeDir = "/users/ug15sl/.local/nix/var";
-    };
-  };
 }

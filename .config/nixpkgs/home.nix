a6c29921a2bf0eb0a623d09b8165f73d368d6dd9
Sauyon Lee <s@uyon.co>
2019-07-09 16:18:28 +0100

    gpg-agent stuff

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -19,19 +19,19 @@
   };
 
   services = {
-    # gpg-agent = {
-    #   enable = true;
-    #   enableSshSupport = true;
-    #   defaultCacheTtl = 600;
-    #   maxCacheTtl = 1200;
-    #   sshKeys = [
-    #     "8F517C9DB64C3501E026B4375F1F39329F92D634"
-    #     "5971188B0E62D499E1F6D72C80C776AB8D6ACC2C"
-    #   ];
-    #   extraConfig = ''
-    #     pinentry-program ${pkgs.pinentry}/bin/pinentry
-    #   '';
-    # };
+    gpg-agent = {
+      enable = true;
+      enableSshSupport = true;
+      defaultCacheTtl = 600;
+      maxCacheTtl = 1200;
+      sshKeys = [
+        "8F517C9DB64C3501E026B4375F1F39329F92D634"
+        "5971188B0E62D499E1F6D72C80C776AB8D6ACC2C"
+      ];
+      extraConfig = ''
+        pinentry-program ${pkgs.pinentry}/bin/pinentry
+      '';
+    };
   };
 
   programs = {

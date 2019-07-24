dd90f9fd308fe7152d692fda063c9eac281ec307
Sauyon Lee <s@uyon.co>
2019-07-24 18:49:06 +0100

    Hide firefox tab bar

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -41,7 +41,25 @@
     };
 
     alacritty = import ./alacritty.nix;
-    firefox.enable = true;
+    firefox = {
+      package = pkgs.firefox-wayland;
+      enable = true;
+      profiles.default = {
+        userChrome = ''
+          /* Hide tab bar in FF Quantum */
+          @-moz-document url("chrome://browser/content/browser.xul") {
+          #TabsToolbar {
+            visibility: collapse !important;
+              margin-bottom: 21px !important;
+            }
+
+          #sidebar-box[sidebarcommand="treestyletab_piro_sakura_ne_jp-sidebar-action"] #sidebar-header {
+            visibility: collapse !important;
+            }
+          }
+        '';
+      };
+    };
 
     emacs.enable = true;
 
